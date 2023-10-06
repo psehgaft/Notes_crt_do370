bad8f503fd2086a540777d424fc26a0447fe9ee0
Heber Romero Tellez <psehgaft@gmail.com>
2023-10-05 23:41:19 -0400

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -3,3 +3,15 @@
 
 oc extract secret/noobaa-registry -n openshift-image-registry
 oc create secret generic image-registry-private-configuration-user --from-literal=REGISTRY_STORAGE_S3_ACCESSKEY=myaccesskey --from-literal=REGISTRY_STORAGE_S3_SECRETKEY=mysecretkey --namespace openshift-image-registry
+
+
+# Edit config/cluster
+
+spec:
+  storage:
+    managementState: Managed
+    pvc: null
+    s3:
+      bucket: noobaa-registry-038ca5ee-d9ed-4b20-997a-c72058af2426
+      region: us-east-1
+      regionEndpoint: https://s3-openshift-storage.apps.ocp4.example.com
